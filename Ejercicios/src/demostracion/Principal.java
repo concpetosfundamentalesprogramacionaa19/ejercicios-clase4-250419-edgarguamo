a591794378070915e40a34aad693802ccf52722d
edgarguamo@gmail.com <edgarguamo@gmail.com>
2019-04-25 12:53:32 -0500

    Condicional if con ejercicio corregido

--- Ejercicios/src/demostracion/Principal.java
+++ Ejercicios/src/demostracion/Principal.java
@@ -21,6 +21,7 @@ public class Principal {
         int mensaje;
         double iva;
         double extra;
+        double operacion;
         Scanner teclado = new Scanner (System.in);
         
         //mensaje al usuario
@@ -31,32 +32,26 @@ public class Principal {
         //condicional if 
         if (mensaje >= 200){
             // para calcular cuantos mensajes son mayores a 200
-            extra = mensaje - 200; 
-            extra = extra * 0.10;
-            tarifa = tarifa + extra;
+            extra = mensaje - 200; //obtener el numero de mensajes >200 
+            operacion = extra * 0.10;//calcular el valor de los mesajes > 200
+            tarifa = tarifa + operacion;
             
             // para obtener el valor de los mensajes entre 200 y 41
             mensaje = 159;
-            extra = mensaje * 0.05;
-            tarifa = tarifa + extra;
+            operacion = mensaje * 0.05; // calcular el valor de mensajes <200
+            tarifa = tarifa + operacion;
         }else{
             if (mensaje > 40 && mensaje <= 200){
                 
                 //para obtener el precio de los mensajes mayores a 40 
-                
                 extra = mensaje -40;
-                extra = mensaje * 0.05;
-                tarifa = tarifa + extra;
-                       
+                operacion = mensaje * 0.05; //calcular el valor de mensajes <200
+                tarifa = tarifa + operacion; 
             }
-        
         }
         iva = tarifa * 0.12;
         tarifa = tarifa + iva;
         System.out.printf("El valor de su tarifa a pagar es de: %.2f", tarifa );
-        
-                   
-            
-        
+     
     }
 }
